6db154d0646ca830adc647992c852cb895b9c47a
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-20 12:57:04 +0200

    Modernize Nix shell definition

--- default.nix
+++ default.nix
@@ -1,19 +1,34 @@
-{ cmd ? "${builtins.toString ./rename_documents.py}" }:
-
-with import <nixpkgs> { };
+# In this .nix file, define a NixOS shell environment with pdfgrep and Python installed
 
 let
-  pythonPackages = python311Packages;
-  project = builtins.baseNameOf ./.;
-in
-pkgs.mkShell rec {
-  name = project;
-  buildInputs = [
-    pdfgrep
-    pythonPackages.python
+  projectName = "docnamer";
+  pythonVersion = "311";
+  home = builtins.getEnv "HOME";
+  venv = "${home}/.virtualenvs/${projectName}";
+  pkgs = import <nixos-unstable> {};
+  docnamer = pkgs."python${pythonVersion}Packages".buildPythonPackage rec {
+    pname = "${projectName}";
+    version = "0.0.1";
+    format = "flit";
+    src = ./.;
+  };
+  my-python-packages = py: [
+    docnamer
+    py.flit
   ];
-
-  shellHook = ''
-    ${cmd}
-  '';
-}
+  my-python = pkgs."python${pythonVersion}".withPackages my-python-packages;
+in (pkgs.buildFHSUserEnv {
+  name = "${projectName}";
+  targetPkgs = pkgs: with pkgs; [
+    git
+    gnupg
+    pdfgrep
+    my-python
+    zsh
+  ];
+  runScript = "
+    rm -f ${venv}
+    ln -s ${my-python} ${venv}
+    cd ${builtins.toString ./.}
+    zsh";
+}).env
