07eb446ab6b4afe255295b553838c2a2de2f4147
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2023-03-20 12:56:58 +0200

    Package it up

---
+++ src/docnamer/__init__.py
@@ -0,0 +1,3 @@
+"""Tool for automatic content-based renaming of document files."""
+
+__version__ = "0.0.1"
---
+++ src/docnamer/__main__.py
@@ -0,0 +1,9 @@
+from src.docnamer import rename_documents
+
+
+def main():
+    rename_documents.main()
+
+
+if __name__ == "__main__":
+    main()
